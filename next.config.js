01ed854f845e09d8c3429e771b44968e0a423cb3
temasaguru <temasaguru@gmail.com>
2023-03-14 13:28:06 +0900

    style: format

--- next.config.js
+++ next.config.js
@@ -9,12 +9,12 @@ const nextConfig = {
   // },
   images: {
     // next/imageコンポーネント用
-    loader: 'custom',
+    loader: "custom",
   },
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer(nextConfig);
